b45df1686905119b878559008722d1b47b858fab
Tamás Gálffy <ezittgtx@gmail.com>
2023-03-21 23:52:09 +0100

    feat: Setup nlon host
    Closes #5

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,36 @@
+/* eslint-disable */
+import * as nlon from '@elementbound/nlon'
+/* eslint-enable */
+
+import * as net from 'node:net'
+import { wrapSocketServer } from '@elementbound/nlon-socket'
+import logger from './logger.mjs'
+import config from './config.mjs'
+
 /**
   * Run app
   */
 function main () {
-  console.log('Hello world!')
+  logger.info('Starting Natty')
+
+  const socket = net.createServer()
+
+  /** @type {nlon.Server} */
+  const nlons = wrapSocketServer(socket, {
+    logger: logger.child({}, { name: 'nlons' })
+  })
+
+  socket.listen(config.socket.port, config.socket.host, () =>
+    logger.info('Listening on %s:%s', config.socket.host, config.socket.port)
+  )
+
+  nlons.handle('echo', async (_peer, corr) => {
+    for await (const message of corr.all()) {
+      corr.write(message)
+    }
+
+    corr.finish()
+  })
 }
 
 main()
