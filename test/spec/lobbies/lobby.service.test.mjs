80a58744ca3d113a008b1363659689ff2e00c8ba
Tamas Galffy <ezittgtx@gmail.com>
2023-04-07 14:20:21 +0200

    Add service tests

---
+++ test/spec/lobbies/lobby.service.test.mjs
@@ -0,0 +1,133 @@
+import { beforeEach, describe, it, mock } from 'node:test'
+import assert from 'node:assert'
+import { LobbyService } from '../../../src/lobbies/lobby.service.mjs'
+import { mockClass } from '../../mocking.mjs'
+import { LobbyRepository } from '../../../src/lobbies/lobby.repository.mjs'
+import { LobbyParticipantRepository } from '../../../src/lobbies/lobby.participant.repository.mjs'
+import { NotificationService } from '../../../src/notifications/notification.service.mjs'
+import { User } from '../../../src/users/user.mjs'
+import { Repository } from '../../../src/repository.mjs'
+import { GameData } from '../../../src/games/game.data.mjs'
+import { LobbyData } from '../../../src/lobbies/lobby.data.mjs'
+
+describe('LobbyService', () => {
+  /** @type {LobbyRepository} */
+  let lobbyRepository
+  /** @type {LobbyParticipantRepository} */
+  let participantRepository
+  /** @type {NotificationService} */
+  let notificationService
+
+  /** @type {LobbyService} */
+  let lobbyService
+
+  beforeEach(() => {
+    lobbyRepository = mockClass(LobbyRepository, Repository)
+    participantRepository = mockClass(LobbyParticipantRepository, Repository)
+    notificationService = mockClass(NotificationService)
+
+    lobbyService = new LobbyService({
+      nameConfig: {
+        minNameLength: 4,
+        maxNameLength: 16
+      },
+      lobbyRepository,
+      participantRepository,
+      notificationService
+    })
+  })
+
+  describe('create', () => {
+    it('should create lobby', () => {
+      // Given
+      const lobbyName = 'test'
+      const ownerUser = new User({ id: 'a', name: 'b' })
+      const game = new GameData({ id: 'foo', name: 'Foo game' })
+
+      participantRepository.getLobbiesOf = mock.fn(() => [])
+      participantRepository.add = mock.fn(item => item)
+      lobbyRepository.add = mock.fn(item => item)
+      notificationService.send = mock.fn(() => [])
+
+      // When
+      const actual = lobbyService.create(lobbyName, ownerUser, game)
+
+      // Then
+      assert(actual)
+      assert.equal(actual.game, game.id)
+      assert.equal(actual.name, lobbyName)
+      assert.equal(actual.owner, ownerUser.id)
+    })
+
+    it('should add owner to new lobby', () => {
+      // Given
+      const lobbyName = 'test'
+      const ownerUser = new User({ id: 'a', name: 'b' })
+      const game = new GameData({ id: 'foo', name: 'Foo game' })
+
+      participantRepository.getLobbiesOf = mock.fn(() => [])
+      participantRepository.getParticipantsOf = mock.fn(lobby => [ownerUser.id])
+      participantRepository.add = mock.fn(item => item)
+      lobbyRepository.add = mock.fn(item => item)
+      notificationService.send = mock.fn(() => [])
+
+      // When
+      lobbyService.create(lobbyName, ownerUser, game)
+
+      // Then
+      assert.equal(notificationService.send.mock.callCount(), 1)
+      assert.deepEqual(
+        notificationService.send.mock.calls[0].arguments[0].userIds,
+        [ownerUser.id]
+      )
+    })
+
+    it('should reject short name', () => {
+      // Given
+      const lobbyName = '.'
+      const ownerUser = new User({ id: 'a', name: 'b' })
+      const game = new GameData({ id: 'foo', name: 'Foo game' })
+
+      // When + then
+      assert.throws(
+        () => lobbyService.create(lobbyName, ownerUser, game),
+        e => e.message === 'Lobby name too short!'
+      )
+    })
+
+    it('should reject long name', () => {
+      // Given
+      const lobbyName = '.'.repeat(128)
+      const ownerUser = new User({ id: 'a', name: 'b' })
+      const game = new GameData({ id: 'foo', name: 'Foo game' })
+
+      // When + then
+      assert.throws(
+        () => lobbyService.create(lobbyName, ownerUser, game),
+        e => e.message === 'Lobby name too long!'
+      )
+    })
+
+    it('should reject when already in lobby', () => {
+      // Given
+      const lobbyName = 'test'
+      const ownerUser = new User({ id: 'a', name: 'b' })
+      const game = new GameData({ id: 'foo', name: 'Foo game' })
+      const currentLobby = new LobbyData({
+        id: '0xtest',
+        game: game.id,
+        name: 'Current lobby',
+        owner: ownerUser.id
+      })
+
+      participantRepository.getLobbiesOf = mock.fn(() => ['0xtest'])
+      lobbyRepository.find = mock.fn(() => currentLobby)
+
+      // When + then
+      assert.throws(
+        () => lobbyService.create(lobbyName, ownerUser, game),
+        e => e.message === 'User is already in a lobby!'
+      )
+    })
+  })
+})
