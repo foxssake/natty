5569d568974805ae6a5af5f7c3d750b3c8d7ae0d
Tamás Gálffy <ezittgtx@gmail.com>
2023-04-11 23:38:37 +0200

    chore: Add leave lobby tests
    Closes #18

--- src/lobbies/subjects/leave.lobby.mjs
+++ src/lobbies/subjects/leave.lobby.mjs
@@ -1,11 +1,14 @@
 /* eslint-disable */
 import { Server } from '@elementbound/nlon'
 import { User } from '../../users/user.mjs'
+import { GameData } from '../../games/game.data.mjs'
 /* eslint-enable */
 import { requireAuthorization } from '../../validators/require.header.mjs'
 import { lobbyParticipantRepository, lobbyRepository, lobbyService } from '../lobbies.mjs'
 import { requireSession } from '../../sessions/validators/require.session.mjs'
 import { requireSessionUser } from '../../sessions/validators/require.session.user.mjs'
+import { requireSessionGame } from '../../sessions/validators/require.session.game.mjs'
+import logger from '../../logger.mjs'
 
 class NotInLobbyError extends Error { }
 
@@ -18,20 +21,39 @@ export function leaveLobbySubject (server) {
     await corr.next(
       requireAuthorization(),
       requireSession(),
-      requireSessionUser()
+      requireSessionUser(),
+      requireSessionGame()
     )
 
     /** @type {User} */
     const user = corr.context.user
+    /** @type {GameData} */
+    const game = corr.context.game
 
-    const lobbyId = lobbyParticipantRepository.getLobbyOf(user.id)
+    const log = logger.child({
+      name: 'leaveLobbySubject',
+      user: user.id,
+      game: game.id,
+      session: corr.context.session.id
+    })
+
+    log.info('User requesting to leave lobby')
+
+    // Find lobby corresponding to session game
+    const lobbyId = lobbyParticipantRepository.getLobbiesOf(user.id)
+      .map(lobbyId => lobbyRepository.find(lobbyId))
+      .find(lobby => lobby?.game === game.id)
+      ?.id
+
     if (!lobbyId) {
+      log.error('No lobby found for user!')
       throw new NotInLobbyError('User is not in any lobby!')
     }
 
     const lobby = lobbyRepository.find(lobbyId)
 
     // Leave
+    log.info({ lobby: lobby.id }, 'Removing user from lobby')
     lobbyService.leave(user, lobby)
 
     corr.finish()
