aefd0c923d8f43b48d32fad27c70e882a7707912
Tamás Gálffy <ezittgtx@gmail.com>
2023-05-27 19:41:06 +0200

    Add constraint methods

--- src/relay/constraints.mjs
+++ src/relay/constraints.mjs
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import { BandwidthLimiter } from './bandwidth.limiter.mjs'
 import { UDPRelayHandler } from './udp.relay.handler.mjs'
 /* eslint-enable */
 import assert from 'node:assert'
@@ -12,5 +13,48 @@ import assert from 'node:assert'
 export function constrainRelayTableSize (relayHandler, maxSize) {
   relayHandler.on('create', () => {
     assert(relayHandler.relayTable.length <= maxSize, 'Relay table size limit reached!')
+  })
+}
+
+/**
+* Limit the bandwidth on every relay individually.
+* @param {UDPRelayHandler} relayHandler Relay handler
+* @param {number} traffic Traffic limit in bytes/sec
+* @param {number} [interval=1] Limit interval in seconds
+*/
+export function constrainIndividualBandwidth (relayHandler, traffic, interval) {
+  const limiters = new Map()
+
+  relayHandler.on('transmit', (source, _target, message) => {
+    if (!limiters.has(source.id)) {
+      limiters.set(source.id, new BandwidthLimiter({
+        maxTraffic: traffic,
+        interval: interval ?? 1
+      }))
+    }
+
+    const limiter = limiters.get(source.id)
+    limiter.validate(message.length)
+  })
+
+  relayHandler.on('destroy', relay => {
+    limiters.delete(relay.id)
+  })
+}
+
+/**
+* Limit the bandwidth on every relay globally.
+* @param {UDPRelayHandler} relayHandler Relay handler
+* @param {number} traffic Traffic limit in bytes/sec
+* @param {number} [interval=1] Limit interval in seconds
+*/
+export function constrainGlobalBandwidth (relayHandler, traffic, interval) {
+  const limiter = new BandwidthLimiter({
+    maxTraffic: traffic,
+    interval: interval ?? 1
+  })
+
+  relayHandler.on('transmit', (source, _target, message) => {
+    limiter.validate(message.length)
   })
 }
---
+++ test/spec/relay/constraints.test.mjs
@@ -0,0 +1,174 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+import sinon from 'sinon'
+import { RelayEntry } from '../../../src/relay/relay.entry.mjs'
+import { NetAddress } from '../../../src/relay/net.address.mjs'
+import { UDPRelayHandler } from '../../../src/relay/udp.relay.handler.mjs'
+import { constrainGlobalBandwidth, constrainIndividualBandwidth, constrainRelayTableSize } from '../../../src/relay/constraints.mjs'
+
+describe('Relay constraints', () => {
+  describe('constrainRelayTableSize', () => {
+    const relayTable = [
+      new RelayEntry({
+        address: new NetAddress({ address: '37.89.0.5', port: 32467 }),
+        port: 10001
+      }),
+
+      new RelayEntry({
+        address: new NetAddress({ address: '57.13.0.9', port: 45357 }),
+        port: 10002
+      }),
+    ]
+
+    it('should allow', () => {
+      // Given
+      const relayHandler = sinon.createStubInstance(UDPRelayHandler)
+      sinon.stub(relayHandler, 'relayTable').value(relayTable)
+      relayHandler.on.callThrough()
+      relayHandler.emit.callThrough()
+
+      constrainRelayTableSize(relayHandler, 2)
+
+      // When + Then
+      assert.doesNotThrow(
+        () => relayHandler.emit('create', relayTable[1])
+      )
+    })
+
+    it('should throw', () => {
+      const relayHandler = sinon.createStubInstance(UDPRelayHandler)
+      sinon.stub(relayHandler, 'relayTable').value(relayTable)
+      relayHandler.on.callThrough()
+      relayHandler.emit.callThrough()
+
+      constrainRelayTableSize(relayHandler, 1)
+
+      // When + Then
+      assert.throws(
+        () => relayHandler.emit('create', relayTable[1])
+      )
+    })
+  })
+
+  describe('constrainIndividualBandwidth', () => {
+    it('should pass', () => {
+      // Given
+      const relayTable = [
+        new RelayEntry({
+          address: new NetAddress({ address: '37.89.0.5', port: 32467 }),
+          port: 10001
+        }),
+
+        new RelayEntry({
+          address: new NetAddress({ address: '57.13.0.9', port: 45357 }),
+          port: 10002
+        }),
+      ]
+
+      const message = Buffer.from('a'.repeat(16))
+
+      const relayHandler = sinon.createStubInstance(UDPRelayHandler)
+      relayHandler.on.callThrough()
+      relayHandler.emit.callThrough()
+
+      constrainIndividualBandwidth(relayHandler, 16)
+
+      // When + Then
+      assert.doesNotThrow(() =>
+        relayHandler.emit('transmit', relayTable[0], relayTable[1], message)
+      )
+    })
+
+    it('should throw on too much data', () => {
+      // Given
+      const relayTable = [
+        new RelayEntry({
+          address: new NetAddress({ address: '37.89.0.5', port: 32467 }),
+          port: 10001
+        }),
+
+        new RelayEntry({
+          address: new NetAddress({ address: '57.13.0.9', port: 45357 }),
+          port: 10002
+        }),
+      ]
+
+      const message = Buffer.from('a'.repeat(32))
+
+      const relayHandler = sinon.createStubInstance(UDPRelayHandler)
+      relayHandler.on.callThrough()
+      relayHandler.emit.callThrough()
+
+      constrainIndividualBandwidth(relayHandler, 16)
+
+      // When + Then
+      assert.throws(() =>
+        relayHandler.emit('transmit', relayTable[0], relayTable[1], message)
+      )
+    })
+  })
+
+  describe('constrainGlobalBandwidth', () => {
+    it('should pass', () => {
+      // Given
+      const relayTable = [
+        new RelayEntry({
+          address: new NetAddress({ address: '37.89.0.5', port: 32467 }),
+          port: 10001
+        }),
+
+        new RelayEntry({
+          address: new NetAddress({ address: '57.13.0.9', port: 45357 }),
+          port: 10002
+        }),
+      ]
+
+      const message = Buffer.from('a'.repeat(4))
+
+      const relayHandler = sinon.createStubInstance(UDPRelayHandler)
+      relayHandler.on.callThrough()
+      relayHandler.emit.callThrough()
+
+      constrainGlobalBandwidth(relayHandler, 16)
+
+      // When + Then
+      assert.doesNotThrow(() =>
+        relayHandler.emit('transmit', relayTable[0], relayTable[1], message)
+      )
+      assert.doesNotThrow(() =>
+        relayHandler.emit('transmit', relayTable[1], relayTable[0], message)
+      )
+    })
+
+    it('should throw', () => {
+      // Given
+      const relayTable = [
+        new RelayEntry({
+          address: new NetAddress({ address: '37.89.0.5', port: 32467 }),
+          port: 10001
+        }),
+
+        new RelayEntry({
+          address: new NetAddress({ address: '57.13.0.9', port: 45357 }),
+          port: 10002
+        }),
+      ]
+
+      const message = Buffer.from('a'.repeat(12))
+
+      const relayHandler = sinon.createStubInstance(UDPRelayHandler)
+      relayHandler.on.callThrough()
+      relayHandler.emit.callThrough()
+
+      constrainGlobalBandwidth(relayHandler, 16)
+
+      // When + Then
+      assert.doesNotThrow(() =>
+        relayHandler.emit('transmit', relayTable[0], relayTable[1], message)
+      )
+      assert.throws(() =>
+        relayHandler.emit('transmit', relayTable[1], relayTable[0], message)
+      )
+    })
+  })
+})
